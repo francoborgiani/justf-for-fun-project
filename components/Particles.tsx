925d4ca7458e93444eb422e2201b93d31383137d
Franco Borgiani <franco@nookalabs.com>
2023-02-05 20:40:24 -0300

    [ADD] particles background and header

---
+++ components/Particles.tsx
@@ -0,0 +1,36 @@
+import styles from "../styles/Particles.module.scss"
+
+export const Particles = () => {
+  return <div className={styles.particleContainer}>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+    <div className={styles.particle}></div>
+  </div>
+}
